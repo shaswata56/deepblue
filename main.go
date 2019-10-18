597449a6c4f4e298e35ff8a4ff11fffa591cadd2
shaswata56 <shaswata56@gmail.com>
2019-10-19 03:17:04 +0600

    transparent proxy in go

--- main.go
+++ main.go
@@ -1,20 +1,54 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"gopkg.in/elazarl/goproxy.v1"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"regexp"
 )
+
+func orPanic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	port := os.Getenv("PORT")
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-	proxy.OnRequest().DoFunc(
-		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			r.Header.Set("X-GoProxy", "shaswata56")
-			return r, nil
+	proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile("^.*$"))).
+		HandleConnect(goproxy.AlwaysMitm)
+	proxy.OnRequest(goproxy.ReqHostMatches(regexp.MustCompile("^.*:80$"))).
+		HijackConnect(func(req *http.Request, client net.Conn, ctx *goproxy.ProxyCtx) {
+			defer func() {
+				if e := recover(); e != nil {
+					ctx.Logf("error connecting to remote: %v", e)
+					client.Write([]byte("HTTP/1.1 500 Cannot reach destination\r\n\r\n"))
+				}
+				client.Close()
+			}()
+			clientBuf := bufio.NewReadWriter(bufio.NewReader(client), bufio.NewWriter(client))
+			remote, err := net.Dial("tcp", req.URL.Host)
+			orPanic(err)
+			remoteBuf := bufio.NewReadWriter(bufio.NewReader(remote), bufio.NewWriter(remote))
+			for {
+				req, err := http.ReadRequest(clientBuf.Reader)
+				orPanic(err)
+				orPanic(req.Write(remoteBuf))
+				orPanic(remoteBuf.Flush())
+				resp, err := http.ReadResponse(remoteBuf.Reader, req)
+				orPanic(err)
+				orPanic(resp.Write(clientBuf.Writer))
+				orPanic(clientBuf.Flush())
+			}
 		})
-	log.Fatal(http.ListenAndServe(":"+port, proxy))
+	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
+	addr := flag.String("", ":"+port, "proxy listening")
+	flag.Parse()
+	proxy.Verbose = *verbose
+	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
